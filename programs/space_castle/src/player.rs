1c43899d7a776f4d7500f7675300c7329cd83afd
Mitja Belak <mico@0x0.icu>
2024-04-09 23:17:27 +0200

    Initial Ship modeling

--- programs/space_castle/src/player.rs
+++ programs/space_castle/src/player.rs
@@ -1,10 +1,12 @@
 use anchor_lang::prelude::*;
+use crate::{seeds, Ship};
 
 #[account]
 #[derive(InitSpace)]
 pub struct Player {
     #[max_len(24)]
     pub name: String,
+    pub num_fleets: u16
 }
 
 #[derive(Accounts)]
@@ -14,10 +16,18 @@ pub struct CreatePlayer<'info> {
     #[account(
         init,
         payer = signer,
-        seeds = [b"player", signer.key().as_ref()], 
+        seeds = [seeds::PLAYER, signer.key().as_ref()], 
         bump,
         space = 8 + Player::INIT_SPACE 
     )]
     pub player: Account<'info, Player>,
+    #[account(
+        init,
+        payer = signer,
+        seeds = [seeds::SHIP, signer.key().as_ref(), b"1"], 
+        bump,
+        space = 8 + Player::INIT_SPACE 
+    )]
+    pub initial_ship: Account<'info, Ship>,
     pub system_program: Program<'info, System>,
 }
--- programs/space_castle/src/ship.rs
+++ programs/space_castle/src/ship.rs
@@ -1,13 +1,43 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct Ship {
-    pub owner: Pubkey,
-    pub ship_type: ShipType,
+    engine: EngineModule,
+    armor: u8,
+    shields: u8,
+    engine_module: EngineModule,
+    #[max_len(3)]
+    weapon_modules: Vec<WeaponModule>,
+    #[max_len(3)]
+    utility_modules: Vec<UtilityModule>,
 }
 
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy)]
-pub enum ShipType {
-    Warship,
-    MiningShip,
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, InitSpace)]
+pub struct WeaponModule {
+    weapon_type: WeaponType,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, InitSpace)]
+pub struct UtilityModule {
+    utility_type: UtilityType,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, InitSpace)]
+pub enum EngineModule {
+    Normal,
+    Fast,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, InitSpace)]
+pub enum WeaponType {
+    Kinetic,
+    Laser,
+    Rocket,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, InitSpace)]
+pub enum UtilityType {
+    ShieldGenerator,
+    EngineBooster,
 }
