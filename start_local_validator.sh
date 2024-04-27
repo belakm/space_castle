52a88b9ae27bd63e005ee0c5eb5c73569e0d79c3
Mitja Belak <mico@0x0.icu>
2024-04-27 15:43:58 +0200

    Moved test ledger location

--- start_local_validator.sh
+++ start_local_validator.sh
@@ -4,4 +4,5 @@ solana-test-validator \
     --reset \
     --url https://api.devnet.solana.com \
     --clone metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s \
+    --ledger ./.anchor/test-ledger \
     --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s metaplex.so
